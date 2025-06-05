f97b35b862c977b85f84ecea83ca46b65f9da068
HanzHKR <kubicek.jan@hotmail.com>
2025-06-05 03:49:05 +0200

    -picker na jednotky, počítání i obsahu kruhu

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,14 +7,19 @@
         public MainPage()
         {
             InitializeComponent();
-
+            JednotkyPicker.ItemsSource = Jednotky;
         }
+
+        public static List<string> Jednotky { get; } = new List<string> { "m", "cm", "mm" };
+
 
         private void VypocitejBtnClicked(object sender, EventArgs e)
         {
             string polomerVstup = PolomerEntry.Text;
             double obvod = Vypocty.VypisObvod(polomerVstup);
+            double obsah = Vypocty.VypisObsah(polomerVstup);
             VysledekObvodLabel.Text = $"Obvod: {obvod}";
+            VysledekObsahLabel.Text = $"Obsah: {obsah}";
         }
     }
     internal static class Vypocty
@@ -32,9 +37,26 @@
                 return 0; 
             }
         }
+        public static double VypisObsah(string polomerText)
+        {
+            if (double.TryParse(polomerText, out double polomer))
+            {
+                return VypocitejObsah(polomer);
+
+
+            }
+            else
+            {
+                return 0;
+            }
+        }
         private static double VypocitejObvod(double polomer)
         {
             return 2 * Math.PI * polomer;
+        }
+        private static double VypocitejObsah(double polomer)
+        {
+            return Math.PI * polomer * polomer;
         }
     }
 }
